c1bb4b5802d9df00b40cc1b3e49974902f54dcdf
SciKa <enzoescipy@gmail.com>
2022-02-08 16:19:53 +0900

    face analyzing by naver clova open API adapted.

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import requests
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+
+import json
+
+client_id = "b_GHhK7YJ9GfvaGW6kCc"
+client_secret = "OGh9kCg2Sg"
+img = 'example21.jpg'
+url = "https://openapi.naver.com/v1/vision/face" 
+files = {'image': open(img, 'rb')}
+headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+response = requests.post(url,  files=files, headers=headers)
+rescode = response.status_code
+if(rescode==200):
+    pass
+else:
+    print("Error Code:" + rescode)
+    raise Exception
+
+response = json.loads(response.text)
+imginfo = response["info"]
+faces = response["faces"]
+
+fig, ax = plt.subplots(figsize=(10,10))
+
+faceRects = []
+faceTexts = []
+
+for face in faces:
+    x = face["roi"]['x']
+    y = face["roi"]['y']
+    w = face["roi"]['width']
+    h = face["roi"]['height']
+
+    facerect = patches.Rectangle( (x,y),w,h ,linewidth = 5, edgecolor = 'g', facecolor="none")
+
+    faceRects.append(facerect)
+
+    textset = [x,y,w,h]
+    gendertext = "gender"+ ":"+ face['gender']['value']+ " ," +str(int(face['gender']['confidence'] * 100)) + "%"
+    textset.append(gendertext)
+    emotext = "emotion" + ":" + face['emotion']['value']+ "," + str(int(face['emotion']["confidence"]* 100)) + "%"
+    textset.append(emotext)
+    agetext = "age" + ":" + face['age']['value']+ "," + str(int(face['age']["confidence"]* 100)) + "%"
+    textset.append(agetext)
+    faceTexts.append(textset)
+    
+
+
+ax.imshow(mpimg.imread(img))
+for facerect in faceRects :
+    ax.add_patch(facerect)
+for text in faceTexts :
+    x = text[0]
+    y = text[1]
+    w = text[2]
+    h = text[3]
+    ax.text(x+10,y+h+10,text[4] + "\n" + text[5] + "\n" + text[6],fontsize=15,color='red',verticalalignment='top')
+
+plt.show()
