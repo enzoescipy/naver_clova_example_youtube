f227e567708bd8ea00f184908c3fea8bd6fcd44c
SciKa <enzoescipy@gmail.com>
2022-07-25 20:04:34 +0900

    slkjdfn

--- main_youtube.py
+++ main_youtube.py
@@ -8,8 +8,9 @@ import json
 
 client_id = "b_GHhK7YJ9GfvaGW6kCc"
 client_secret = "OGh9kCg2Sg"
-img = 'example21.jpg'
 url = "https://openapi.naver.com/v1/vision/face" 
+img = "example.jpg"
+
 files = {'image': open(img, 'rb')}
 headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
 response = requests.post(url,  files=files, headers=headers)
@@ -21,43 +22,45 @@ else:
     raise Exception
 
 response = json.loads(response.text)
-imginfo = response["info"]
-faces = response["faces"]
-
-fig, ax = plt.subplots(figsize=(10,10))
+imginfo = response['info']
+faces = response['faces']
 
 faceRects = []
 faceTexts = []
 
-for face in faces:
-    x = face["roi"]['x']
-    y = face["roi"]['y']
-    w = face["roi"]['width']
-    h = face["roi"]['height']
 
-    facerect = patches.Rectangle( (x,y),w,h ,linewidth = 5, edgecolor = 'g', facecolor="none")
+for face in faces:
+    x = face['roi']['x']
+    y = face['roi']['y']
+    w = face['roi']['width']
+    h = face['roi']['height']
+    facerect = patches.Rectangle( (x,y) ,w,h, linewidth = 5, edgecolor = 'g', facecolor='none')
 
     faceRects.append(facerect)
 
     textset = [x,y,w,h]
-    gendertext = "gender"+ ":"+ face['gender']['value']+ " ," +str(int(face['gender']['confidence'] * 100)) + "%"
+    gendertext = "gender" + ":" + face['gender']['value'] + ' ,' + str(int(face['gender']['confidence'] * 100)) + "%"
     textset.append(gendertext)
-    emotext = "emotion" + ":" + face['emotion']['value']+ "," + str(int(face['emotion']["confidence"]* 100)) + "%"
-    textset.append(emotext)
-    agetext = "age" + ":" + face['age']['value']+ "," + str(int(face['age']["confidence"]* 100)) + "%"
-    textset.append(agetext)
-    faceTexts.append(textset)
-    
 
+    emotiontext = "emotion" + ":" + face['emotion']['value'] + ' ,' + str(int(face['emotion']['confidence'] * 100)) + "%"
+    textset.append(emotiontext)
+
+    agetext = "age" + ":" + face['age']['value'] + ' ,' + str(int(face['age']['confidence'] * 100)) + "%"
+    textset.append(agetext)
+
+    faceTexts.append(textset)
+
+fig, ax = plt.subplots(figsize = (10,10))
 
 ax.imshow(mpimg.imread(img))
 for facerect in faceRects :
     ax.add_patch(facerect)
-for text in faceTexts :
+
+for text in faceTexts:
     x = text[0]
     y = text[1]
     w = text[2]
     h = text[3]
-    ax.text(x+10,y+h+10,text[4] + "\n" + text[5] + "\n" + text[6],fontsize=15,color='red',verticalalignment='top')
-
+    ax.text(x+10, y+h+10, text[4] + '\n'+text[5]+'\n'+text[6] , fontsize = 15, color = 'red', verticalalignment = 'top')
 plt.show()
+
